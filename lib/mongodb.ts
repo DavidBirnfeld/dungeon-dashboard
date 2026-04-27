e90fec2145deae13c4f3fac8f08e5feff2f4c59c
David Birnfeld <davidbirnfeld@gmail.com>
2026-04-27 09:36:47 +0300

    Update dependencies: Upgrade Next.js to ^9.3.3 and add MongoDB ^7.2.0

---
+++ lib/mongodb.ts
@@ -0,0 +1,33 @@
+import { MongoClient } from "mongodb";
+
+declare global {
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
+
+const state = process.env.NODE_ENV;
+const uri = process.env.MONGODB_URI;
+const options = {};
+
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+if (!uri) {
+  throw new Error("Please add your Mongo URI to .env.local");
+}
+
+if (state === "production") {
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+} else {
+  if (_mongoClientPromise) {
+    clientPromise = _mongoClientPromise;
+  } else {
+    client = new MongoClient(uri, options);
+    clientPromise = client.connect();
+    global._mongoClientPromise = clientPromise;
+  }
+}
+
+// Export a module-scoped MongoClient promise. By doing this in a
+// separate module, the client can be shared across functions.
+export default clientPromise;
