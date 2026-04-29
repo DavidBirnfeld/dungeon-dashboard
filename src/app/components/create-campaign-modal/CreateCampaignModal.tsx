1caffb2fb7d1fda6ca340f0b9495170e8ec58061
David Birnfeld <davidbirnfeld@gmail.com>
2026-04-29 21:23:19 +0300

    feat: add form handling for campaign creation in CreateCampaignModal

--- src/app/components/create-campaign-modal/CreateCampaignModal.tsx
+++ src/app/components/create-campaign-modal/CreateCampaignModal.tsx
@@ -1,4 +1,5 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
+
 export function CreateCampaignModal({
   isOpen,
   onClose,
@@ -6,28 +7,46 @@ export function CreateCampaignModal({
   isOpen: boolean;
   onClose: () => void;
 }) {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Form Submitted!");
+    onClose();
+  };
   const dialogRef = useRef<HTMLDialogElement>(null);
 
   useEffect(() => {
     if (dialogRef.current) {
-        if (isOpen && !dialogRef.current.open) {
-            dialogRef.current.showModal()
-        } else {
-            dialogRef.current.close()
-        }
+      if (isOpen && !dialogRef.current.open) {
+        dialogRef.current.showModal();
+      } else {
+        dialogRef.current.close();
+      }
     }
   }, [isOpen]);
   return (
-    <dialog ref={dialogRef} onClose={() => onClose()}>
-      <button type="button" onClick={() => onClose()}>
+    <dialog ref={dialogRef} onClose={onClose}>
+      <button type="button" onClick={onClose}>
         X
       </button>
-      <form action="">
+      <form action="" onSubmit={handleSubmit}>
         <label htmlFor="campaign-name">Campaign Name:</label>
-        <input type="text" id="campaign-name" />
+        <input
+          type="text"
+          id="campaign-name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
 
         <label htmlFor="campaign-description">Description</label>
-        <input type="text" id="campaign-description" />
+        <input
+          type="text"
+          id="campaign-description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button type="submit">Create</button>
       </form>
     </dialog>
   );
